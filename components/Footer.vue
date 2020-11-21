6b38a76851a2d9f3e69b2916b27ed9b57eab6f91
kamalius <kennichekamal@gmail.com>
2020-11-21 15:07:22 +0100

    modif nuxtconfig.js

--- components/Footer.vue
+++ components/Footer.vue
@@ -54,6 +54,82 @@
             ></path>
           </svg>
         </a>
+        <a href="https://www.youtube.com/channel/UCcOEMzu58baVGunSW524T6Q">
+          <svg
+            id="Layer_1"
+            version="1.1"
+            viewBox="0 0 500 500"
+            fill="none"
+            xml:space="preserve"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            class="h-6 w-6 fill-current ml-3"
+            data-v-7ac1059d=""
+          >
+            <g data-v-7ac1059d="">
+              <linearGradient
+                id="SVGID_1_"
+                gradientUnits="userSpaceOnUse"
+                x1="29.8503"
+                x2="443.0858"
+                y1="48.8522"
+                y2="462.0877"
+                data-v-7ac1059d=""
+              >
+                <stop
+                  offset="0"
+                  style="stop-color: currentColor"
+                  data-v-7ac1059d=""
+                ></stop>
+                <stop
+                  offset="0.5153"
+                  style="stop-color: currentColor"
+                  data-v-7ac1059d=""
+                ></stop>
+                <stop
+                  offset="1"
+                  style="stop-color: currentColor"
+                  data-v-7ac1059d=""
+                ></stop>
+              </linearGradient>
+              <polygon
+                fill="currentColor"
+                points="217.949,313.149 313.196,256 217.949,198.851  "
+                data-v-7ac1059d=""
+              ></polygon>
+              <linearGradient
+                id="SVGID_2_"
+                gradientUnits="userSpaceOnUse"
+                x1="39.3513"
+                x2="452.5868"
+                y1="39.3513"
+                y2="452.5867"
+                data-v-7ac1059d=""
+              >
+                <stop
+                  offset="0"
+                  style="stop-color: currentColor"
+                  data-v-7ac1059d=""
+                ></stop>
+                <stop
+                  offset="0.5153"
+                  style="stop-color: currentColor"
+                  data-v-7ac1059d=""
+                ></stop>
+                <stop
+                  offset="1"
+                  style="stop-color: currentColor"
+                  data-v-7ac1059d=""
+                ></stop>
+              </linearGradient>
+              <path
+                d="M256,7.098C118.535,7.098,7.098,118.535,7.098,256c0,137.465,111.437,248.902,248.902,248.902   c137.465,0,248.902-111.437,248.902-248.902C504.902,118.535,393.465,7.098,256,7.098z M408.349,268.287   c0,26.307-3.048,52.605-3.048,52.605s-2.981,22.402-12.106,32.251c-11.592,12.935-24.574,13.011-30.527,13.773   C320.026,370.183,256,370.297,256,370.297s-79.227-0.771-103.601-3.267c-6.782-1.353-21.993-0.962-33.594-13.897   c-9.134-9.849-12.106-32.251-12.106-32.251s-3.048-26.289-3.048-52.605v-24.65c0-26.298,3.048-52.596,3.048-52.596   s2.981-22.412,12.106-32.279c11.601-12.935,24.574-13.011,30.527-13.744c42.633-3.305,106.592-3.305,106.592-3.305h0.143   c0,0,63.959,0,106.601,3.305c5.953,0.733,18.935,0.81,30.527,13.744c9.134,9.868,12.106,32.279,12.106,32.279   s3.048,26.298,3.048,52.605V268.287z"
+                fill="currentColor"
+                data-v-7ac1059d=""
+              ></path>
+            </g>
+          </svg>
+        </a>
       </span>
     </div>
   </footer>
