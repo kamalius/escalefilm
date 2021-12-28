727947ca5a05c7817869736a919adb6462d6c111
kamalius <kennichekamal@gmail.com>
2021-12-28 11:26:23 +0100

    mise a jour darkmode

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,7 @@
         </div>
         <div class="hidden lg:block">
           <div class="flex flex-wrap items-center">
-            <div class="mr-2"><DarkMode /></div>
+            <div class="mr-5"><DarkMode /></div>
             <div class="items-end">
               <nav
                 class="md:ml-auto flex flex-wrap items-center text-base justify-end"
@@ -36,7 +36,7 @@
         </div>
         <div class="block lg:hidden items-center">
           <div class="flex flex-wrap items-center">
-            <div class="mr-2"><DarkMode /></div>
+            <div class="mr-5"><DarkMode /></div>
             <button
               class="focus:outline-none"
               aria-label="open menu"
